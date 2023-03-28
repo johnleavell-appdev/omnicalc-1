97f7225f5c3eb85fc3a2a5885241bfc9efeefa97
John Leavell <johnleavell@gmail.com>
2023-03-28 20:58:51 +0000

    built out missing routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,32 @@
 class ApplicationController < ActionController::Base
-
   def blank_square_form
-
-    render({ :template => "calculation_templates/square_form.html.erb"})
+    render({ :template => "calculation_templates/square_form.html.erb" })
   end
 
   def calculate_square
     @num = params.fetch("pillow").to_f
 
     @square_of_num = @num * @num
-    render({ :template => "calculation_templates/square_results.html.erb"})
+    render({ :template => "calculation_templates/square_results.html.erb" })
   end
 
+  def calculate_random
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+
+    render({ :template => "calculation_templates/rand_results.html.erb" })
+  end
+
+  def blank_random_form
+    render({ :template => "calculation_templates/rand_form.html.erb" })
+  end
+
+  def blank_payment_form
+    render({ :template => "calculation_templates/payment_form.html.erb" })
+  end
+
+  def square_root_form
+    render({ :template => "calculation_templates/square_root_form.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get("/square/new", { :controller => "application", :action => "blank_square_form" })
   get("/square/results", { :controller => "application", :action => "calculate_square" })
+  get("/random/results", { :controller => "application", :action => "calculate_random" })
+  get("/random/new", { :controller => "application", :action => "blank_random_form"})
+  get("/payment/new", { :controller => "application", :action => "blank_payment_form"})
+  get("/square_root/new", { :controller => "application", :action => "square_root_form"})
 end
